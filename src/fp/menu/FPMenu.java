9a59c582f12253a34989130dc51fb9c211f3a68c
AlejandroSanchezPonce <A01410366@itesm.mx>
2016-10-03 21:31:06 -0500

    Update FPMenu.java

--- src/fp/menu/FPMenu.java
+++ src/fp/menu/FPMenu.java
@@ -18,4 +18,63 @@ public class FPMenu {
         // TODO code application logic here
     }
     
+System.out.println("La opcion elegida es: " +  menu());    
+    }
+    public static int menu(){
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        do{
+        System.out.println("**** Calculo de areas ****");
+        System.out.println("**** Menu ****");
+        System.out.println("1. Area de cuadrados");
+        System.out.println("2. Area de circulos");
+        System.out.println("3. Area de triangulos");
+        System.out.println("0. Salir");
+        System.out.print("Introdusca la opcion deseada: ");
+        opcion = entrada.nextInt();
+        }while(opcion<0 || opcion>3);
+        return opcion;
+    }
+    public static float calcularArea(int op){
+        float area;
+        switch (op){
+            case 1:
+                area = areaCuadrado();
+                break;
+            case 2:
+                area = areaCirculo();
+                break;
+            case 3:
+                area = areaTriangulo();
+                break;
+            default:
+                mensajeSalida();
+        }
+    public static double solicitarDatos(String tipo){
+        double dato;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introduzca el valor de " + tipo + " : ");
+        dato = entrada.nextDouble();
+        return dato;
+    }
+        if ("opcion".equals(1)){
+            
+            public static float areaCuadrado(){
+                float area;
+                double a;
+                a = solicitarDatos("un lado");
+                area = (float) Math.pow(a, 2);
+                return area;
+        }
+        if ("opcion".equals(2)){   
+            public static float areaCirculo(){
+            float area;
+            double r;
+            final double pi;
+            pi = 3.14;
+            r = solicitarDatos("el radio");
+            area = (float) (pi*Math.pow(r, 2));
+            return area;
+    }
 }
+
